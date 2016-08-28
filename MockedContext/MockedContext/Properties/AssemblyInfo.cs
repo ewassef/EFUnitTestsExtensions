80f0e9493f77549f5c2acc6381643752e277209d
EWASSEF-LAPTOP\eddie <Eddie Wassef>
2016-08-28 18:23:37 -0500

    added nuspec

--- MockedContext/MockedContext/Properties/AssemblyInfo.cs
+++ MockedContext/MockedContext/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MockedContext")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Eddie Wassef")]
 [assembly: AssemblyProduct("MockedContext")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
